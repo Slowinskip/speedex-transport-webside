2fa4a3ae51405b87ef5c0e79134c56f9300f7f17
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-24 17:11:40 +0200

    add html code and style to baner components

--- src/components/views/Banner/Banner.js
+++ src/components/views/Banner/Banner.js
@@ -1,7 +1,23 @@
 import React from "react";
 import styles from "./Banner.module.scss";
 const Banner = () => {
-  return <div className={styles.banner_background}></div>;
+  return (
+    <>
+      <div className={styles.banner_background}></div>
+      <div className={styles.baner_content}>
+        <img src="./images/logo_white.png" alt="img"></img>
+        <h1>Taking Logistics to Another Level</h1>
+        <p>
+          A new player in the transport industry. Trust Speedex and entrust us
+          with your shipment
+        </p>
+        <div className={styles.content_btn}>
+          <button className={styles.btn1}>What Sets us Appart?</button>
+          <button className={styles.btn2}>List of Out Services</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Banner;
