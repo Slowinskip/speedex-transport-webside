bb5af6ab05a9560b99f152d6fcdfa8fcf53c108c
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-24 18:42:05 +0200

    Add styles to service component

--- src/components/views/Service/Service.js
+++ src/components/views/Service/Service.js
@@ -4,14 +4,14 @@ import styles from "./Service.module.scss";
 const Service = () => {
   return (
     <section>
-      <Container>
+      <Container className={styles.container}>
         <div className={styles.servie_title}>
           <p>SERVICE</p>
           <h1>We provide full assistance in forwarding and storage</h1>
         </div>
-        <Row xs={1} md={2} lg={3}>
+        <Row xs={1} md={2} lg={3} className={styles.row_content}>
           <Col className={styles.srvice_box}>
-            <img alt="img" src="./images/"></img>
+            <img alt="img" src="./images/Plane.png"></img>
             <h3>Air Freight</h3>
             <p>
               By ordering air transport, you know that the shipment will arrive
@@ -19,7 +19,7 @@ const Service = () => {
             </p>
           </Col>
           <Col className={styles.srvice_box}>
-            <img alt="img" src="./images/"></img>
+            <img alt="img" src="./images/Ship.png"></img>
             <h3>Ocean Freight</h3>
             <p>
               Water transport is recommended for large-size shipments, such as a
@@ -27,7 +27,7 @@ const Service = () => {
             </p>
           </Col>
           <Col className={styles.srvice_box}>
-            <img alt="img" src="./images/"></img>
+            <img alt="img" src="./images/Truck.png"></img>
             <h3>Land Express</h3>
             <p>
               Ground transport will easily transport your package between nearby
