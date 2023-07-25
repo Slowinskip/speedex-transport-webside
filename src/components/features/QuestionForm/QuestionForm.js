0fc086b4b8d33653ef176304cb3d4165d7fb514b
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-25 19:57:51 +0200

    Add styles to QuestionForm component

--- src/components/features/QuestionForm/QuestionForm.js
+++ src/components/features/QuestionForm/QuestionForm.js
@@ -11,53 +11,70 @@ const QuestionForm = () => {
         <Row xs={1} md={2}>
           <Col className={"" + styles.questionForm}>
             <h1>Need to Make an Enquiry?</h1>
-            <input
-              className={"col-5 " + styles.questionForm_input}
-              type="text"
-              placeholder="Your name*"
-            />
-            <input
-              className={"col-5 " + styles.questionForm_input}
-              type="text"
-              placeholder="Affiliation*"
-            />
-            <input
-              className={"col-5 " + styles.questionForm_input}
-              type="emaik"
-              placeholder="Email*"
-            />
-            <input
-              className={"col-5 " + styles.questionForm_input}
-              type="number"
-              placeholder="Number*"
-            />
-            <input
-              className={"col-10 " + styles.questionForm_input}
-              type="text"
-              placeholder="Topic*"
-            />{" "}
-            <textarea
-              className={"col-10 " + styles.questionForm_input}
-              placeholder="Your Message*"
-            />
-            <button className={"col-10 " + styles.questionForm_btn}>
-              Submit Message
-            </button>
+            <div className={"" + styles.inputContainer}>
+              <input
+                className={"col-11 col-md-5 " + styles.questionForm_input}
+                type="text"
+                placeholder="Your name*"
+              />
+              <input
+                className={"col-11 col-md-5 " + styles.questionForm_input}
+                type="text"
+                placeholder="Affiliation*"
+              />
+              <input
+                className={"col-11 col-md-5 " + styles.questionForm_input}
+                type="email"
+                placeholder="Email*"
+              />
+              <input
+                className={"col-11 col-md-5 " + styles.questionForm_input}
+                type="number"
+                placeholder="Number*"
+              />
+              <input
+                className={"col-11 " + styles.questionForm_input}
+                type="text"
+                placeholder="Topic*"
+              />{" "}
+              <textarea
+                className={"col-11 " + styles.questionForm_textArea}
+                placeholder="Your Message*"
+              />
+              <button className={"col-11 " + styles.questionForm_btn}>
+                Submit Message
+              </button>
+            </div>
           </Col>
           <Col className={"" + styles.questionInformation}>
             <h6>GET IN TOUCH WITH US</h6>
             <h1>Our company will take care of your transport</h1>
-            <p>
+            <p className={styles.questionInformationText}>
               Speedex has many years of experience in the transport and storage
-              of goods. We have hundreds of satisfied customers
+              of goods. We have hundreds of satisfied customers. Contact us to
+              learn about all the advantages of our cooperation.
             </p>
             <h4>Wrocław</h4>
-            <BiMap />
-            <p>53-027 Wrocław, Januszowicka 99/99 Street</p>
-            <AiOutlineMail />
-            <p>speedexoffice@speedex.com</p>
-            <AiOutlinePhone />
-            <p>123-456-789</p>
+            <div className={styles.questionInformation_date}>
+              <div>
+                <span>
+                  <BiMap className={styles.questionInformation_icon} />
+                </span>
+                <p>53-027 Wrocław, Januszowicka 99/99 Street</p>
+              </div>
+              <div>
+                <span>
+                  <AiOutlineMail className={styles.questionInformation_icon} />
+                </span>
+                <p>speedexoffice@speedex.com</p>
+              </div>
+              <div>
+                <span>
+                  <AiOutlinePhone className={styles.questionInformation_icon} />
+                </span>
+                <p>123-456-789</p>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
