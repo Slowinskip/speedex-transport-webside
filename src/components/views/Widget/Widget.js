ef66a1735c07559afd09754c868e09eb47f7206c
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-26 20:36:52 +0200

    Add styles and media display to widget component

--- src/components/views/Widget/Widget.js
+++ src/components/views/Widget/Widget.js
@@ -6,36 +6,44 @@ const Widget = () => {
     <section>
       <div className={styles.widgetBackground}>
         <Container>
-          <Row xs={1} md={3}>
+          <Row xs={1} md={2} lg={3} className={styles.row}>
             <Col className={styles.widgetBox}>
-              <Card>
+              <Card className={styles.card}>
                 <Card.Img variant="top" src="./images/store.jpg"></Card.Img>
-                <Card.Title>Storage spaces tailored to your needs</Card.Title>
-                <Card.Text>
-                  We have 6 transshipment centers located throughout the country
+                <Card.Title className={styles.card_Title}>
+                  Storage spaces tailored to your needs
+                </Card.Title>
+                <Card.Text className={styles.card_Text}>
+                  We have 6 wireless transshipments throughout the country
+                  country. Thanks to the support of logistics partners around
+                  the world, we will meet all challenges{" "}
                 </Card.Text>
               </Card>
             </Col>
             <Col className={styles.widgetBox}>
               {" "}
-              <Card>
+              <Card className={styles.card}>
                 <Card.Img
                   variant="top"
                   src="./images/TruckWidget.jpg"
                 ></Card.Img>
-                <Card.Title>Newly purchased fleet</Card.Title>
-                <Card.Text>
-                  We have a large range of cars from trucks to buses to meet
-                  your expectations
+                <Card.Title className={styles.card_Title}>
+                  Newly purchased fleet
+                </Card.Title>
+                <Card.Text className={styles.card_Text}>
+                  We have users with many years of experience equipping them
+                  with the latest equipment{" "}
                 </Card.Text>
               </Card>
             </Col>
             <Col className={styles.widgetBox}>
               {" "}
-              <Card>
+              <Card className={styles.card}>
                 <Card.Img variant="top" src="./images/employer.jpg"></Card.Img>
-                <Card.Title>We focus on our employees</Card.Title>
-                <Card.Text>
+                <Card.Title className={styles.card_Title}>
+                  We focus on our employees
+                </Card.Title>
+                <Card.Text className={styles.card_Text}>
                   We believe that taking care of employee development and
                   satisfaction translates into long-term cooperation
                 </Card.Text>
